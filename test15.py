10fd6573df6c255695331d07c028889a907cfae8
lghxiayan <lghxiayan@163.com>
2021-11-24 09:30:38 +0800

    test15.py

---
+++ test15.py
@@ -0,0 +1,10 @@
+#!/usr/bin/conda python
+# -*- coding: utf-8 -*-
+# @Time : 2021/11/24 8:44
+# @Author : xiayan
+# @Email : lghxiayan@163.com
+
+
+import sys
+
+print("RECLIMIT:{}, EXEPATH:{}, UNICODE:{}".format(sys.getrecursionlimit(), sys.executable, sys.maxunicode))
