60f2725caecdd9085aaa6d8d9f407d4b15d62bdb
lghxiayan <lghxiayan@163.com>
2021-12-07 20:48:33 +0800

    test15.py

--- test18.py
+++ test18.py
@@ -1,4 +1,22 @@
-import pizza
+class User:
+    """用户类"""
 
-pizza.make_pizza(16, 'pepperoni')
-pizza.make_pizza(12, 'mushrooms', 'green peppers', 'extra cheese')
+    def __init__(self, first, last):
+        """初始化first_name和last_name等两个属性"""
+        self.first_name = first
+        self.last_name = last
+
+    def describe_user(self):
+        """打印用户信息摘要"""
+        print(f"This man first name is {self.first_name}")
+        print(f"and last name is {self.last_name}")
+
+    def greet_user(self):
+        """个性的问候"""
+        print(f"Hello {self.last_name} {self.first_name}")
+
+
+tom = User('tom', 'jack')
+print(tom.first_name, tom.last_name)
+tom.describe_user()
+tom.greet_user()
