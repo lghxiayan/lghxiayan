d31f83ae062a2d023f8b162de9b1a5cbebe63328
lghxiayan <lghxiayan@163.com>
2021-12-09 16:17:09 +0800

    test15.py

--- test15.py
+++ test15.py
@@ -1,16 +1,16 @@
 class Restaurant:
     """餐厅类"""
 
-    def __init__(self, name, type):
+    def __init__(self, restaurant_name, cuisine_type):
         """初始化属性"""
-        self.restaurant_name = name
-        self.icusine_type = type
+        self.restaurant_name = restaurant_name
+        self.cuisine_type = cuisine_type
         self.number_served = 0
-        self.IceCreamStand = IceCreamStand()
+        self.ice_cream_stand = IceCreamStand()
 
-    def descirbe_restaurant(self):
+    def describe_restaurant(self):
         """描述餐厅"""
-        long_name = f'{self.restaurant_name} 是一家 {self.icusine_type}'
+        long_name = f'{self.restaurant_name} 是一家 {self.cuisine_type}'
         print(long_name)
 
     def open_restaurant(self):
@@ -33,22 +33,27 @@ class Restaurant:
 class IceCreamStand:
     """冰激凌类"""
 
-    def __init__(self, flavors):
+    def __init__(self, *flavors):
         """初始化"""
         self.flavors = flavors
 
     def disp_flavors(self):
         """显示冰激凌的口味"""
-        print(f"冰激凌的口味有：{self.flavors}")
+        print(f"冰激凌的口味有：")
+        for i in list(self.flavors):
+            for j in i:
+                print(f"- {j}")
+        # print(list(self.flavors), type(self.flavors))
 
 
 ct1 = Restaurant('北京莫斯科餐厅', '西餐厅')
-ct1.descirbe_restaurant()
+ct1.describe_restaurant()
 ct1.open_restaurant()
 ct1.get_number_served()
 ct1.set_number_served(35)
 ct1.get_number_served()
 ct1.increment_number_served(100)
 ct1.get_number_served()
-ice1 = IceCreamStand('草苺味')
-ct1.IceCreamStand.disp_flavors()
+# ct1.ice_cream_stand.disp_flavors()
+ice1 = IceCreamStand(['口味4', '口味5', '口味6'])
+ice1.disp_flavors()
