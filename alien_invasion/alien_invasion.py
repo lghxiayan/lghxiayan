1bc0057b20081ed98fe9b3983fbfc503d82e8eb2
lghxiayan <lghxiayan@163.com>
2021-12-21 14:54:51 +0800

    test15.py

---
+++ alien_invasion/alien_invasion.py
@@ -0,0 +1,41 @@
+import sys
+import pygame
+
+from settings import Settings
+from ship import Ship
+
+
+class AlienInvasion:
+    """管理游戏资源和行为的类"""
+
+    def __init__(self):
+        """初始化游戏并创建游戏资源"""
+        pygame.init()
+        self.settings = Settings()
+
+        self.screen = pygame.display.set_mode(
+            (self.settings.screen_width, self.settings.screen_height))
+        pygame.display.set_caption(self.settings.screen_caption)
+        # 设置背景色
+        self.bg_color = self.settings.bg_color
+
+        self.ship = Ship(self)
+
+    def run_game(self):
+        """开始游戏的主循环"""
+        while True:
+            # 监视键盘和鼠标事件
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+            # 每次循环都会重绘屏幕
+            self.screen.fill(self.bg_color)
+            self.ship.blitme()
+
+            # 让最近绘制的屏幕可见
+            pygame.display.flip()
+
+
+if __name__ == '__main__':
+    ai = AlienInvasion()
+    ai.run_game()
---
+++ alien_invasion/settings.py
@@ -0,0 +1,10 @@
+class Settings:
+    """存储游戏《外星人入侵》中所有设置的类"""
+
+    def __init__(self):
+        """初始化"""
+        # 屏幕设置
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (230, 230, 230)
+        self.screen_caption = "Alien Invasion"
